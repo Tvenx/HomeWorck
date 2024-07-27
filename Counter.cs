f9332b2983644cf393e13f951861e712424eefc6
Павел <128207591+Tvenx@users.noreply.github.com>
2024-07-27 13:41:23 +0300

    Update Counter.cs

--- Counter.cs
+++ Counter.cs
@@ -1,37 +1,34 @@
 using System.Collections;
-using TMPro;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
-   [SerializeField] private float _interval = 0.5f;
+    private float _counterValue = 0f;
+    private float _interval = 0.5f;
 
-   [SerializeField] private int _addNumber = 1;
-    private int _count = 0;
+    public float addValue = 1f;
+
+    public float CounterValue => _counterValue;
 
     private bool isCounting = false;
-
-    public TextMeshProUGUI _counterTextMeshPro;
-
-    private IEnumerator Start()
-    {
-        while (true)
-        {
-            if (isCounting)
-            {
-                _count += _addNumber;
-                _counterTextMeshPro.text = _count.ToString("");
-                Debug.Log("Count: " + _count);
-            }
-
-            yield return new WaitForSeconds(_interval);
-        }
-    }
 
     public void OnMouseDown()
     {
         isCounting = !isCounting;
+
+        if (isCounting)
+            StartCoroutine(IncrementCounter());
+
+        if (!isCounting)
+            StopCoroutine(IncrementCounter());
     }
 
-   
+    private IEnumerator IncrementCounter()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_interval);
+            _counterValue += addValue;
+        }
+    }
 }
