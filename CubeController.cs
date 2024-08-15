5809471e1ec8dc7845f9ca8a3619902692651d6d
Павел <128207591+Tvenx@users.noreply.github.com>
2024-08-15 17:12:54 +0300

    Update CubeController.cs

--- CubeController.cs
+++ CubeController.cs
@@ -6,6 +6,9 @@ public class CubeController : MonoBehaviour
     private CubeController _cubePrefab;
 
     [SerializeField] private float _explosionForce = 10f;
+    [SerializeField] private float _spawnRadius = 2f;
+    [SerializeField] private float _scaleFactor = 0.5f;
+
     [SerializeField] private int _minRange = 2;
     [SerializeField] private int _maxRange = 6;
 
@@ -16,7 +19,7 @@ public class CubeController : MonoBehaviour
 
     private void OnMouseDown()
     {
-        CubeSpawner.SpawnCubes(_minRange, _maxRange, _explosionForce, _cubePrefab, transform);
+        CubeSpawner.SpawnCubes(_minRange, _maxRange, _explosionForce, _spawnRadius, _scaleFactor, _cubePrefab, transform);
         Destroy(gameObject);
     }
 }
