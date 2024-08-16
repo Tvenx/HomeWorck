de803dc9c4277738cbb9b9b0e271b4e0c647ce08
Павел <128207591+Tvenx@users.noreply.github.com>
2024-08-16 12:13:13 +0300

    Add files via upload

---
+++ CubeInteractor .cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+public class CubeInteractor : MonoBehaviour
+{
+    private CubeInteractor _cubePrefab;
+
+    [SerializeField] private float _explosionForce = 10f;
+    [SerializeField] private float _spawnRadius = 2f;
+    [SerializeField] private float _scaleFactor = 0.5f;
+
+    [SerializeField] private int _minRange = 2;
+    [SerializeField] private int _maxRange = 6;
+
+    private void Start()
+    {
+        _cubePrefab = this;
+    }
+
+    private void OnMouseDown()
+    {
+        CubeSpawner.SpawnCubes(_minRange, _maxRange, _explosionForce, _spawnRadius, _scaleFactor, _cubePrefab, transform);
+        Destroy(gameObject);
+    }
+}
+
+
